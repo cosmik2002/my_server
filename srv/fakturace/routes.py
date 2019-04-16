6459d3200a47ee53a6e2d56d396dbc44d03a9b5d
nicholas <2007.nicholas@gmail.com>
2019-04-16 17:49:23 +0300

    *facturace - more queries (load_only, with_entities)

--- srv/fakturace/routes.py
+++ srv/fakturace/routes.py
@@ -5,13 +5,17 @@ from srv import db
 from flask_login import login_required,current_user
 from flask_table import create_table, Table, Col
 from sqlalchemy import desc
+from sqlalchemy.orm import load_only
 import pandas
+
 
 
 @bp.route('/')
 @bp.route('/index')
+@bp.route('/index/<int:page>')
 @login_required
-def index():
+def index(page=1):
+    per_page = 10
     class ItemTable(Table):
         cislofa = Col('cislofa')
         numobjednavka = Col('numobjednavka')
@@ -27,9 +31,13 @@ def index():
     #           dict(name='Name3', description='Description3')]
     # df = pandas.read_sql(knihafaktur.query.join(adresy).order_by(desc(knihafaktur.cislofa))
     #                      .add_columns(adresy.nazevfirmy).limit(50).statement,db.get_engine(bind='fakturace'))
-    df = pandas.read_sql(knihafaktur.query.join(adresy).order_by(desc(knihafaktur.cislofa))
-                         .from_self(knihafaktur.cislofa,adresy.nazevfirmy).limit(10).statement,db.get_engine(bind='fakturace'))
-    #df = pandas.DataFrame(knihafaktur.query.join(adresy).order_by(desc(knihafaktur.cislofa)).limit(50))
+    stmt = knihafaktur.query.order_by(desc(knihafaktur.cislofa)).from_self(knihafaktur.cislofa,adresy.nazevfirmy).\
+        join(adresy).statement
+    stmt = knihafaktur.query.order_by(desc(knihafaktur.cislofa)).options(load_only("cislofa")).join("adresy").statement
+    stmt = knihafaktur.query.order_by(desc(knihafaktur.cislofa)).join("adresy").with_entities(knihafaktur.cislofa,adresy.nazevfirmy).statement
+    df = pandas.read_sql(knihafaktur.query.order_by(desc(knihafaktur.cislofa)).from_self(knihafaktur.cislofa,adresy.nazevfirmy).
+                         join(adresy).limit(per_page).offset(page*per_page).statement,db.get_engine(bind='fakturace'))
+    df = pandas.read_sql(stmt, db.get_engine(bind='fakturace'))
     table_cls_pd = create_table(options={'border': 'solid'})
     for col in df.columns:
         table_cls_pd.add_column(col, Col(col))
@@ -38,4 +46,4 @@ def index():
     table = table_cls(items)
     table1 = ItemTable(items)
     table2 = table_cls_pd(df.to_dict('rows'))
-    return render_template('table.html', table=table2, table1=table)
+    return render_template('fakturace.html', table=table2)
